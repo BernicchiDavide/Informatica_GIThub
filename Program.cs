35e29a0da2f04152cc0bad0c35efb085e53a8527
BernicchiDavide <bernicchi.davidemaria@franchettisalviani.net>
2025-02-11 13:22:41 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int binario(string m)
+{
+    int s = 0;
+    for (int i = m.Length - 1; i >= 0; i--)
+    {
+        if (m[i] == '1')
+        {
+            s += (int)Math.Pow(2, m.Length - 1 - i);
+        }
+    }
+    return s;
+}
+
+Console.WriteLine("dimmi il numero in binario:");
+string m = Console.ReadLine();
+Console.WriteLine($"\n-il numero binario equivale a: {binario(m)}");
