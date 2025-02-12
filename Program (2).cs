ab6144aa2c0e858352bb95a0f27c537f15cd0550
BernicchiDavide <bernicchi.davidemaria@franchettisalviani.net>
2025-02-12 08:35:47 +0100

    Create Program (2).cs

---
+++ Program (2).cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("dimmi la frase");
+string r =  Console.ReadLine();
+char[] v = new char[r.Length];
+for (int i = 0; i < v.Length; i++)
+{
+    v[i]= r[i];
+}
+Console.WriteLine(" ");
+for (int i = 0; i < v.Length / 2; i++)
+{
+    Console.Write(v[i]);
+}
+
+Console.WriteLine("\n ");
+for (int i = v.Length/ 2; i < v.Length; i++)
+{
+    Console.Write(v[i]);
+}
+
+Console.WriteLine(" ");
