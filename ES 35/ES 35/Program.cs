5d438edc399e45f306e8dcc354c570b3d62ad7fd
BernicchiDavide <bernicchi.davidemaria@franchettisalviani.net>
2025-02-17 13:01:53 +0100

    ES 35

---
+++ ES 35/ES 35/Program.cs
@@ -0,0 +1,24 @@
+﻿bool controllo_parentesi(string e)
+{
+    bool lool=false;
+    int p=0;
+    for (int i=0; i<e.Length; i++)
+    {
+        if (e[i] == '(')
+        {
+            p++;
+        }
+        if (e[i] == ')')
+        {
+            p--;
+        }
+    }
+    if (p == 0)
+    {
+        lool= true;
+    }
+    return lool;
+}
+Console.WriteLine("Dimmi l'espressione");
+string e= Console.ReadLine();
+Console.WriteLine($"parentesi = {controllo_parentesi(e)}");
