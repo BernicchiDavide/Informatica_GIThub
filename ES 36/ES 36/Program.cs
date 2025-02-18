3c0bdd4f9ec63488765a5005ff806dd87a9befaf
BernicchiDavide <bernicchi.davidemaria@franchettisalviani.net>
2025-02-18 12:40:31 +0100

    ES 36

---
+++ ES 36/ES 36/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+void casuali(int[,] m)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Random random = new Random();
+        int a = random.Next(0, 5);
+        int b = random.Next(0, 5);
+        m[a,b] = -1;
+    }
+}
+void visualizza_matrice(int[,] m)
+{
+    for (int i = 0;i < m.GetLength(0); i++)
+    {
+        Console.WriteLine("");
+        for (int j = 0; j < m.GetLength(1); j++)
+        {
+            if (m[i,j] == 0)
+            {
+                Console.Write($"[ {m[i, j]}]");
+            }
+            if (m[i,j] == -1)
+            {
+                Console.Write($"[{m[i, j]}]");
+            }
+        }
+    }
+}
+
+int[,] m = { {0,0,0,0,0},
+             {0,0,0,0,0},
+             {0,0,0,0,0},
+             {0,0,0,0,0},
+             {0,0,0,0,0}};
+casuali(m);
+visualizza_matrice(m);
+
