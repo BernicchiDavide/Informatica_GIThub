db88c4f44df816abc8236509aabb9c3c912d957f
BernicchiDavide <bernicchi.davidemaria@franchettisalviani.net>
2025-03-05 11:03:31 +0100

    ES 40

--- ES 34/ES 34/Program.cs
+++ ES 34/ES 34/Program.cs
@@ -1,20 +1,24 @@
 ﻿bool controllo_palindromo(char[] v)
 {
     char[] c = new char[v.Length];
-    int m = v.Length-1;
+    int m = v.Length;
     bool lools=false;
-    for (int i = 0; i <= v.Length-1; i++)
+    for (int i = 0; i < v.Length; i++)
     {
         c[i] = v[i];
     }
-    for (int i = 0; i < v.Length-1; i++)
+    for (int i = 0; i <= v.Length-1; i++)
     {
         m--;
-        if (v[i] == c[i])
+        Console.WriteLine(v[i]);
+        Console.WriteLine(" ");
+        Console.WriteLine(c[m]);
+
+        if (v[i] == c[m])
         {
             lools = true;
         }
-        else if (v[i] != c[i])
+        else if (v[i] != c[m])
         {
             lools = false;
         }
